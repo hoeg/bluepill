733b9f16cbf4827010c55bf6c507e0c61995e66c
Hoeg <psh@lunar.app>
2024-04-29 18:11:17 +0200

    external

--- internal/morpheus/http.go
+++ internal/morpheus/http.go
@@ -3,50 +3,44 @@ package morpheus
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/hoeg/bluepill/internal/values"
 	"k8s.io/api/admission/v1beta1"
-	networkingv1 "k8s.io/api/networking/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func (a *API) Mutate(admReview v1beta1.AdmissionReview) (v1beta1.AdmissionReview, error) {
-	if admReview.Request == nil {
-		return v1beta1.AdmissionReview{}, fmt.Errorf("missing request field")
+type API struct {
+	whitlistedIP values.Whitelist
+	enforce      bool
+}
+
+func NewAPI(config EnforcementConfig) *API {
+	return &API{
+		whitlistedIP: config.Whitelist,
+		enforce:      config.Enforce,
+	}
+}
+
+func (a *API) PostPill(c *gin.Context) {
+	body, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		return
+	}
+	defer c.Request.Body.Close()
+
+	admReview := v1beta1.AdmissionReview{}
+	if err := json.Unmarshal(body, &admReview); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Internal Server Error - unmarshaling request failed with %s", err)})
+		return
 	}
 
-	resp := &v1beta1.AdmissionResponse{}
-	resp.Allowed = true
-	resp.UID = admReview.Request.UID
-
-	ingress := networkingv1.Ingress{}
-	if err := json.Unmarshal(admReview.Request.Object.Raw, &ingress); err != nil {
-		return v1beta1.AdmissionReview{}, fmt.Errorf("unable to unmarshal object")
+	admReview, err = a.Mutate(admReview)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Internal Server Error - mutation failed with %s", err)})
+		return
 	}
-
-	external := ingress.ObjectMeta.Labels["external"] != ""
-	if a.enforce && !external {
-		resp.AuditAnnotations = map[string]string{
-			"bluepill": "internal enforced",
-		}
-		pt := v1beta1.PatchTypeJSONPatch
-		resp.PatchType = &pt
-
-		p := []map[string]string{
-			{
-				"op":    "add",
-				"path":  "/metadata/annotations/nginx.ingress.kubernetes.io~1whitelist-source-range",
-				"value": a.whitlistedIP.Value(),
-			},
-		}
-		var err error
-		resp.Patch, err = json.Marshal(p)
-		if err != nil {
-			return v1beta1.AdmissionReview{}, fmt.Errorf("Internal Server Error - marshaling response failed with %s", err)
-		}
-	}
-	resp.Result = &metav1.Status{
-		Status: "Success",
-	}
-	admReview.Response = resp
-	return admReview, nil
+	c.JSON(http.StatusOK, admReview)
 }
--- internal/morpheus/kubernetes.go
+++ internal/morpheus/kubernetes.go
@@ -3,44 +3,50 @@ package morpheus
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-	"github.com/hoeg/bluepill/internal/values"
 	"k8s.io/api/admission/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type API struct {
-	whitlistedIP values.Whitelist
-	enforce      bool
-}
-
-func NewAPI(config EnforcementConfig) *API {
-	return &API{
-		whitlistedIP: config.Whitelist,
-		enforce:      config.Enforce,
-	}
-}
-
-func (a *API) PostPill(c *gin.Context) {
-	body, err := io.ReadAll(c.Request.Body)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-		return
-	}
-	defer c.Request.Body.Close()
-
-	admReview := v1beta1.AdmissionReview{}
-	if err := json.Unmarshal(body, &admReview); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Internal Server Error - unmarshaling request failed with %s", err)})
-		return
+func (a *API) Mutate(admReview v1beta1.AdmissionReview) (v1beta1.AdmissionReview, error) {
+	if admReview.Request == nil {
+		return v1beta1.AdmissionReview{}, fmt.Errorf("missing request field")
 	}
 
-	admReview, err = a.Mutate(admReview)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("Internal Server Error - mutation failed with %s", err)})
-		return
+	resp := &v1beta1.AdmissionResponse{}
+	resp.Allowed = true
+	resp.UID = admReview.Request.UID
+
+	ingress := networkingv1.Ingress{}
+	if err := json.Unmarshal(admReview.Request.Object.Raw, &ingress); err != nil {
+		return v1beta1.AdmissionReview{}, fmt.Errorf("unable to unmarshal object")
 	}
-	c.JSON(http.StatusOK, admReview)
+
+	external := ingress.ObjectMeta.Labels["external"] != ""
+	if a.enforce && !external {
+		resp.AuditAnnotations = map[string]string{
+			"bluepill": "internal enforced",
+		}
+		pt := v1beta1.PatchTypeJSONPatch
+		resp.PatchType = &pt
+
+		p := []map[string]string{
+			{
+				"op":    "add",
+				"path":  "/metadata/annotations/nginx.ingress.kubernetes.io~1whitelist-source-range",
+				"value": a.whitlistedIP.Value(),
+			},
+		}
+		var err error
+		resp.Patch, err = json.Marshal(p)
+		if err != nil {
+			return v1beta1.AdmissionReview{}, fmt.Errorf("Internal Server Error - marshaling response failed with %s", err)
+		}
+	}
+	resp.Result = &metav1.Status{
+		Status: "Success",
+	}
+	admReview.Response = resp
+	return admReview, nil
 }
